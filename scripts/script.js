c0771ea63beb9f07beaf4cfe42775195eff4e690
Daria Lazarchuk <dashal24@yandex.ru>
2022-10-14 20:18:53 +0300

    feat: validation

--- scripts/script.js
+++ scripts/script.js
@@ -5,16 +5,17 @@ const cardsSection = document.querySelector('.cards');
 
 const buttonOpenProfileEditForm = document.querySelector('.profile__edit-button');
 const popupEditProfile = document.querySelector('.popup_type_edit-profile');
-const formEditProfile = popupEditProfile.querySelector('form');
-const newUserName = formEditProfile.querySelector('.popup__input_type_name');
-const newUserOccupation = formEditProfile.querySelector('.popup__input_type_occupation');
+const formEditProfile = document.forms.user;
+const newUserName = formEditProfile.elements.username;
+const newUserOccupation = formEditProfile.elements.occupation;
 const buttonCloseEditProfile = popupEditProfile.querySelector('.popup__close-icon');
 
 const buttonAddCardForm = document.querySelector('.profile__add-button');
 const popupNewCard = document.querySelector('.popup_type_new-card');
-const formAddNewCard = popupNewCard.querySelector('form');
-const newCardName = formAddNewCard.querySelector('.popup__input_type_card-name');
-const newCardLink = formAddNewCard.querySelector('.popup__input_type_card-link');
+const formAddNewCard = document.forms.card;
+const newCardName = formAddNewCard.elements.cardname;
+const newCardLink = formAddNewCard.elements.link;
+
 const buttonCloseNewCard = popupNewCard.querySelector('.popup__close-icon');
 
 const popupFullImage = document.querySelector('.popup_type_full-image');
@@ -22,15 +23,21 @@ const fullImage = popupFullImage.querySelector('.popup__image');
 const fullImageCaption = popupFullImage.querySelector('.popup__caption');
 const buttonCloseFullImage = popupFullImage.querySelector('.popup__close-icon');
 
+const openedPopup = document.querySelector('.popup_opened');
+
 
 // открытие попапов
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
+  document.addEventListener('keydown', closePopupByEscape);
+  window.addEventListener('click', closePopupByClickOnOverlay);
 }
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closePopupByEscape);
+  window.removeEventListener('click', closePopupByClickOnOverlay);
 }
 
 
@@ -82,6 +89,9 @@ function submitEditProfileForm(evt) {
   userName.textContent = newUserName.value;
   userOccupation.textContent = newUserOccupation.value;
   closePopup(popupEditProfile);
+  evt.submitter.classList.add('popup__button_disabled');
+  evt.submitter.setAttribute('disabled', true);
+  formEditProfile.reset();
 }
 
 function submitNewCardForm(evt) {
@@ -89,7 +99,28 @@ function submitNewCardForm(evt) {
   cardItem = createCard(newCardLink.value, newCardName.value);
   cardsSection.prepend(cardItem);
   closePopup(popupNewCard);
+  evt.submitter.classList.add('popup__button_disabled');
+  evt.submitter.setAttribute('disabled', true);
+  formAddNewCard.reset();
 }
+
+
+// закрытие по esc
+
+function closePopupByEscape(evt) {
+  if (evt.key === 'Escape') {
+    closePopup(openedPopup);
+  }
+};
+
+
+// закрытие кликом по оверлею
+
+function closePopupByClickOnOverlay(evt) {
+  if (evt.target.classList.contains('popup_opened')) {
+    closePopup(evt.target);
+  }
+};
 
 
 // открытие попапов
@@ -112,6 +143,8 @@ buttonCloseEditProfile.addEventListener('click', () => { closePopup(popupEditPro
 buttonCloseNewCard.addEventListener('click', () => { closePopup(popupNewCard) });
 
 buttonCloseFullImage.addEventListener('click', () => { closePopup(popupFullImage) });
+
+window.addEventListener('click', closePopupByClickOnOverlay);
 
 
 //сабмиты редактирования профиля
