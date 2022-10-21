baa3fcb9bfa37a9fbac09e0e65531b26442ca329
Daria Lazarchuk <dashal24@yandex.ru>
2022-10-21 14:45:05 +0300

    refactor: add classes for card/form-valirator

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
+
+import { Card } from './Card.js';
+import { FormValidator } from './FormValidator.js';
+
 const userName = document.querySelector('.profile__name');
 const userOccupation = document.querySelector('.profile__occupation');
 const cardsSection = document.querySelector('.cards');
@@ -21,6 +25,51 @@ const popupFullImage = document.querySelector('.popup_type_full-image');
 const fullImage = popupFullImage.querySelector('.popup__image');
 const fullImageCaption = popupFullImage.querySelector('.popup__caption');
 const buttonCloseFullImage = popupFullImage.querySelector('.popup__close-icon');
+let cardItem = cardTemplate.querySelector(".cards__item").cloneNode(true);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const settings = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+};
+
+
+function handleCardClick(name, link) {
+  fullImage.alt = `Фотография: ${name}`;
+  fullImage.src = link;
+  fullImageCaption.textContent = name;
+  openPopup(popupFullImage);
+}
 
 
 // открытие попапов
@@ -46,22 +95,27 @@ function createCard(link, name) {
   const cardName = cardItem.querySelector('.cards__name');
   const likeButton = cardItem.querySelector('.cards__like');
   const buttonDeleteCard = cardItem.querySelector('.cards__delete-button');
+
   cardImage.src = link;
   cardImage.alt = `Фотография: ${name}`;
   cardName.textContent = name;
+
   buttonDeleteCard.addEventListener('click', () => {
     const cardItemToDelete = buttonDeleteCard.closest('.cards__item');
     cardItemToDelete.remove();
   });
+
   likeButton.addEventListener('click', (event) => {
     event.target.classList.toggle('cards__like_active');
   });
+
   cardImage.addEventListener('click', () => {
     fullImage.src = cardImage.src;
     fullImage.alt = cardImage.alt;
     fullImageCaption.textContent = cardName.textContent;
     openPopup(popupFullImage);
   });
+
   return cardItem;
 }
 
@@ -108,7 +162,7 @@ function closePopupByEscape(evt) {
     const openedPopup = document.querySelector('.popup_opened');
     closePopup(openedPopup);
   }
-};
+}
 
 
 // закрытие кликом по оверлею
@@ -117,29 +171,31 @@ function closePopupByClickOnOverlay(evt) {
   if (evt.target.classList.contains('popup_opened')) {
     closePopup(evt.target);
   }
-};
+}
 
 
 // открытие попапов
 
 buttonOpenProfileEditForm.addEventListener('click', () => {
   renderProfileEditForm();
+  profileEditFormValidator.hideErrors();
   openPopup(popupEditProfile);
 });
 
 buttonAddCardForm.addEventListener('click', () => {
   resetAddCardForm();
+  newCardFormValidator.hideErrors();
   openPopup(popupNewCard);
 });
 
 
 //закрытие попапов
 
-buttonCloseEditProfile.addEventListener('click', () => { closePopup(popupEditProfile) });
+buttonCloseEditProfile.addEventListener('click', () => { closePopup(popupEditProfile); });
 
-buttonCloseNewCard.addEventListener('click', () => { closePopup(popupNewCard) });
+buttonCloseNewCard.addEventListener('click', () => { closePopup(popupNewCard); });
 
-buttonCloseFullImage.addEventListener('click', () => { closePopup(popupFullImage) });
+buttonCloseFullImage.addEventListener('click', () => { closePopup(popupFullImage); });
 
 window.addEventListener('click', closePopupByClickOnOverlay);
 
@@ -154,6 +210,16 @@ formAddNewCard.addEventListener('submit', submitNewCardForm);
 // карточки из шаблона
 
 initialCards.forEach((element) => {
-  cardItem = createCard(element.link, element.name);
+  const card = new Card(element, '#card__template', handleCardClick);
+  const cardItem = card.generateCard();
   cardsSection.append(cardItem);
 });
+
+
+// экземпляры класса для проверяемых форм
+
+const profileEditFormValidator = new FormValidator(settings, formEditProfile);
+profileEditFormValidator.enableValidation();
+
+const newCardFormValidator = new FormValidator(settings, formAddNewCard);
+newCardFormValidator.enableValidation();
